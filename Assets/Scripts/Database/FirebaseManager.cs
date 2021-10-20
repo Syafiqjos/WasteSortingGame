3665fffad39e2d1b30091669a87e981c70db3e4a
Syafiqjos <syafiqjos@gmail.com>
2021-10-20 19:43:31 +0700

    Fix firebase force quit bug

--- Assets/Scripts/Database/FirebaseManager.cs
+++ Assets/Scripts/Database/FirebaseManager.cs
@@ -8,22 +8,55 @@ using Firebase.Extensions;
 
 public class FirebaseManager : IDatabase
 {
+    FirebaseApp app;
     FirebaseFirestore db;
 
     public void Initialize()
     {
+        InitializeOptions();
+        /*
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
         {
             DependencyStatus status = task.Result;
             if (status == DependencyStatus.Available)
             {
-                db = FirebaseFirestore.DefaultInstance;
-                Debug.Log("Firebase Initialized");
+                InitializeOptions();
             } else
             {
                 Debug.Log("Could not resolve all firebase dependencies: " + status);
             }
         });
+        */
+    }
+
+    private void InitializeOptions()
+    {
+        AppOptions options = AppOptions.LoadFromJsonConfig("{\n  \"project_info\": {\n    \"project_number\": \"548301806630\",\n    \"project_id\": \"wastesortinggame\",\n\t\"firebase_url\": \"https://wastesortinggame-default-rtdb.firebaseio.com\",\n    \"storage_bucket\": \"wastesortinggame.appspot.com\"\n  },\n  \"client\": [\n    {\n      \"client_info\": {\n        \"mobilesdk_app_id\": \"1:548301806630:android:8576e8854fd0ebfb2f9ae7\",\n        \"android_client_info\": {\n          \"package_name\": \"com.rizakusuma.WasteSortingGame\"\n        }\n      },\n      \"oauth_client\": [\n        {\n          \"client_id\": \"548301806630-ms9fut3i83m1q98b3d2rt1qpcflm9204.apps.googleusercontent.com\",\n          \"client_type\": 3\n        }\n      ],\n      \"api_key\": [\n        {\n          \"current_key\": \"AIzaSyDrEZE2qzxi8XrQ-6xHPVgM5RR3ZJ5mCyk\"\n        }\n      ],\n      \"services\": {\n        \"appinvite_service\": {\n          \"other_platform_oauth_client\": [\n            {\n              \"client_id\": \"548301806630-ms9fut3i83m1q98b3d2rt1qpcflm9204.apps.googleusercontent.com\",\n              \"client_type\": 3\n            }\n          ]\n        }\n      }\n    },\n    {\n      \"client_info\": {\n        \"mobilesdk_app_id\": \"1:548301806630:android:e18dd25123b4a1412f9ae7\",\n        \"android_client_info\": {\n          \"package_name\": \"com.rizakusuma.wastesortinggame\"\n        }\n      },\n      \"oauth_client\": [\n        {\n          \"client_id\": \"548301806630-ms9fut3i83m1q98b3d2rt1qpcflm9204.apps.googleusercontent.com\",\n          \"client_type\": 3\n        }\n      ],\n      \"api_key\": [\n        {\n          \"current_key\": \"AIzaSyDrEZE2qzxi8XrQ-6xHPVgM5RR3ZJ5mCyk\"\n        }\n      ],\n      \"services\": {\n        \"appinvite_service\": {\n          \"other_platform_oauth_client\": [\n            {\n              \"client_id\": \"548301806630-ms9fut3i83m1q98b3d2rt1qpcflm9204.apps.googleusercontent.com\",\n              \"client_type\": 3\n            }\n          ]\n        }\n      }\n    }\n  ],\n  \"configuration_version\": \"1\"\n}");
+        /*
+        AppOptions options = new AppOptions()
+        {
+            ApiKey = "AIzaSyDsrEZE2qzxi8XrQ-6xHPVgM5RR3ZJ5mCyk",
+            AppId = "548301806630",
+            DatabaseUrl = new System.Uri("https://wastesortinggame-default-rtdb.firebaseio.com"),
+            ProjectId = "wastesortinggame",
+            StorageBucket = "wastesortinggame.appspot.com"
+        };
+        */
+
+        app = FirebaseApp.Create(options);
+        // app = db.App;
+
+        Debug.Log("ApiKey: " + app.Options.ApiKey);
+        Debug.Log("AppId: " + app.Options.AppId);
+        Debug.Log("DatabaseUrl: " + app.Options.DatabaseUrl.AbsoluteUri);
+        Debug.Log("MessageSender: " + app.Options.MessageSenderId);
+        Debug.Log("ProjectId: " + app.Options.ProjectId);
+        Debug.Log("StorageBucket: " + app.Options.StorageBucket);
+
+        db = FirebaseFirestore.GetInstance(app);
+        // db = FirebaseFirestore.DefaultInstance;
+
+        Debug.Log("Firebase Initialized");
     }
 
     public bool IsInitialized()
